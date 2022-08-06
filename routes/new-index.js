18ab5e8f7cc5e08f5658d393182887d0c6cce87e
chayan0619 <52231215+chayan0619@users.noreply.github.com>
2022-08-06 18:30:35 +0530

    Add files via upload

---
+++ routes/new-index.js
@@ -0,0 +1,23 @@
+
+var express = require('express');
+var router = express.Router();
+
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb+srv://chayan:Chayan@1999@cluster0.e9czy.mongodb.net/?retryWrites=true&w=majority";
+
+
+router.get('/', (req, res, next) => {
+	MongoClient.connect(url, function(err, db) {
+		if (err) throw err;
+		var dbo = db.db("employees");
+		dbo.collection("collection1").find({}).toArray(function(err, result) {
+			if (err) throw err;
+			console.log('Mongo data coming in hot')
+    		console.log(result);
+    		res.json(result)
+    		db.close();
+    	});
+	}); 
+});
+
+module.exports = router;
